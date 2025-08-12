7df17223c7a0523d47529324f5fac71d4ff2a4f9
chube-coder-2k4 <qhuyddbmt123@gmail.com>
2025-08-12 20:13:37 +0700

    draft entity

---
+++ src/main/java/com/carstoremanagement/CarStoreManagementApplication.java
@@ -0,0 +1,13 @@
+package com.carstoremanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarStoreManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarStoreManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/carstoremanagement/CarStoreManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carstoremanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarStoreManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
